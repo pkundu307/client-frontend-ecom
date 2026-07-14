790e0edfc9772a85d685d7efb6a08512a6328a2d
pkundu307 <pkundu307@gmail.com>
2026-07-14 17:09:17 +0530

    for test

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,6 +63,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* ✅ Razorpay moved here, removed the duplicate from the bottom */}
